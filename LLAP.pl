c17b1a2f455b98bd3ce2f2d65eab98ad1b1153e0
Mohamed Gallab <mohamed.w.gallab@gmail.com>
2023-11-28 12:49:44 +0200

    resembles successor states more closely now

--- LLAP.pl
+++ LLAP.pl
@@ -1,7 +1,7 @@
 :- include('KB.pl').
 
 goal(S):-
-    ids(S, 1).
+    ids(S, 5).
 
 ids(X, L):-
     (call_with_depth_limit(goal2(X),L,R), number(R));
@@ -9,46 +9,44 @@ ids(X, L):-
     L1 is L+1, ids(X,L1)).
 
 goal2(S):-
+    S = result(b1, _),
+    succ_state(S, _, _, _, 1, 1);
+    S = result(b2, _),
+    succ_state(S, _, _, _, 1, 1).
+
+succ_state(s0, F, M, E, 0, 0):-
     food(F),
     materials(M),
-    energy(E),
-    helper(s0, F, M, E, 0, 0, S).
+    energy(E).
 
-helper(S, _, _, _, 1, 1, S).
-
-helper(S, F, M, E, 0, B2, S1):-
-    S2 = result(b1, S),
+succ_state(result(b1, S), F, M, E, 1, B2):-
+    succ_state(S, F1, M1, E1, 0, B2),
     build1(Fc, Mc, Ec),
-    F1 is F - Fc,
-    F1 >= 0,
-    M1 is M - Mc,
-    M1 >= 0,
-    E1 is E - Ec,
-    E1 >= 0,
-    helper(S2, F1, M1, E1, 1, B2, S1).
+    F is F1 - Fc,
+    F >= 0,
+    M is M1 - Mc,
+    M >= 0,
+    E is E1 - Ec,
+    E >= 0.
 
-helper(S, F, M, E, B1, 0, S1):-
-    S2 = result(b2, S),
+succ_state(result(b2, S), F, M, E, B1, 1):-
+    succ_state(S, F1, M1, E1, B1, 0),
     build2(Fc, Mc, Ec),
-    F1 is F - Fc,
-    F1 >= 0,
-    M1 is M - Mc,
-    M1 >= 0,
-    E1 is E - Ec,
-    E1 >= 0,
-    helper(S2, F1, M1, E1, B1, 1, S1).
+    F is F1 - Fc,
+    F >= 0,
+    M is M1 - Mc,
+    M >= 0,
+    E is E1 - Ec,
+    E >= 0.
 
-helper(S, F, M, E, B1, B2, S1):-
-    S2 = result(reqf, S),
-    F1 is F + 1,
-    helper(S2, F1, M, E, B1, B2, S1).
+succ_state(result(reqf, S), F, M, E, B1, B2):-
+    succ_state(S, F1, M, E, B1, B2),
+    F is F1 + 1.
 
-helper(S, F, M, E, B1, B2, S1):-
-    S2 = result(reqm, S),
-    M1 is M + 1,
-    helper(S2, F, M1, E, B1, B2, S1).
+succ_state(result(reqm, S), F, M, E, B1, B2):-
+    succ_state(S, F, M1, E, B1, B2),
+    M is M1 + 1.
 
-helper(S, F, M, E, B1, B2, S1):-
-    S2 = result(reqe, S),
-    E1 is E + 1,
-    helper(S2, F, M, E1, B1, B2, S1).
+succ_state(result(reqe, S), F, M, E, B1, B2):-
+    succ_state(S, F, M, E1, B1, B2),
+    E is E1 + 1.
